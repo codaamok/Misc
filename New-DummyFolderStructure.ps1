9a511764929aa4e242ac55d52e55f6134e93f0e4
Adam Cook <adam@cookuop.co.uk>
2019-05-27 16:13:50 +0100

    Initial commit

---
+++ New-DummyFolderStructure.ps1
@@ -0,0 +1,16 @@
+﻿function New-DummyFolderStructure { 
+    Param(
+        [string]$Path,
+        [int]$Long,
+        [int]$Deep
+    )
+    For($LongCounter = 0; $LongCounter -lt $Long; $LongCounter++) {
+        $Folder = ([guid]::NewGuid()).Guid
+        $NewDir = New-Item -ItemType Directory -Path "$Path\$Folder"
+        while ($Deep -gt 1) {
+            $Folder = ([guid]::NewGuid()).Guid
+            $NewDir = New-Item -ItemType Directory -Path "$NewDir\$Folder"
+            $Deep--
+        }
+    }
+}
